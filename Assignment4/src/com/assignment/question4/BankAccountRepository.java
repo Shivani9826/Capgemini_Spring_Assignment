b6d9fb37ffd14066fa9c458f14467b813ceb7bfd
SHIVANI <shivanitiwarist12@gmail.com>
2023-03-06 05:38:12 +0530

    Spring Assignment 4 using another method.

---
+++ Assignment4/src/com/assignment/question4/BankAccountRepository.java
@@ -0,0 +1,10 @@
+package com.assignment.question4;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankAccountRepository {
+	double getBalance(long accountId);
+
+	double updateBalance(long accountId, double newBalance);
+}
